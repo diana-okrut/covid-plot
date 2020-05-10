abcd6077d0ec82af85ceff185890342e2cd825d6
diana-okrut <IHateYOu2014>
2020-05-10 18:55:29 +0300

    statistic graph

---
+++ covid.py
@@ -0,0 +1,24 @@
+import plotly.graph_objects as go
+import pandas as pd
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/opendataby/stopcovid-19/master/stopcovid_data.csv')
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df.date_pub,
+                         y=df.died,
+                         mode='lines+markers',
+                         name='died'))
+fig.add_trace(go.Scatter(x=df.date_pub,
+                         y=df.recovered,
+                         mode='lines+markers',
+                         name='recovered'))
+fig.add_trace(go.Scatter(x=df.date_pub,
+                         y=df.infected,
+                         mode='lines+markers',
+                         name='infected'))
+fig.add_trace(go.Scatter(x=df.date_pub,
+                         y=df.tested,
+                         mode='lines+markers',
+                         name='tested'))
+
+fig.show()
