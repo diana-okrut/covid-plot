66adcb9dc9e84bd648ea21432882a60686f73633
Diana Okrut <zyndel94@gmail.com>
2020-05-14 12:40:58 +0300

    added statistic .png file

--- covid.py
+++ covid.py
@@ -1,14 +1,13 @@
 import plotly.graph_objects as go
 import pandas as pd
+from time import strftime
 
 
 df = pd.read_csv(
     "https://raw.githubusercontent.com/opendataby/stopcovid-19/master/stopcovid_data.csv"
 )
 fig = go.Figure()
-fig.add_trace(
-    go.Scatter(x=df.date_pub, y=df.died, mode="lines+markers", name="died")
-)
+fig.add_trace(go.Scatter(x=df.date_pub, y=df.died, mode="lines+markers", name="died"))
 fig.add_trace(
     go.Scatter(x=df.date_pub, y=df.recovered, mode="lines+markers", name="recovered")
 )
@@ -19,4 +18,5 @@ fig.add_trace(
     go.Scatter(x=df.date_pub, y=df.tested, mode="lines+markers", name="tested")
 )
 
-fig.show()
+YYYYMMDD = strftime("%Y%m%d")
+fig.write_image(f"results/сovid-by-{YYYYMMDD}.png")
